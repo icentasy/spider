071a05b41b459d2b70daed33ef182b41f3b1d3aa
icentasy <545049627@qq.com>
2015-03-17 23:43:31 +0800

    [feature]add spider func

--- spider.py
+++ spider.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 
 PhantomJS_PATH = r'C:\Users\Administrator\Desktop\phantomjs-2.0.0-windows\bin\phantomjs'
 crawl_url = 'http://www.tuan800.com/%s/%s/%i'
+CITY_LIST = ['wuhan']
+TYPE_LIST = ['meishitianxia']
 
 def init_driver():
 	return webdriver.PhantomJS(executable_path=PhantomJS_PATH)
@@ -19,6 +21,10 @@ def download_source(driver, url):
 	return driver.page_source
 
 
+def save2mysql():
+	pass
+
+
 def get_content(source):
 	soup = BeautifulSoup(source)
 	big_div = soup.find('div', attrs={'class': 'deal_980 l bigdeal'})
@@ -26,4 +32,20 @@ def get_content(source):
 	deal_divs.extend(big_div.findAll('div', attrs={'class': 'deal dealbig'}))
 	for deal_div in deal_divs:
 		imag = deal_div.find('img').attrs['src']
+	save2mysql()
+	return
 		
+
+def main():
+	driver = init_driver()
+	for city in CITY_LIST:
+		for _type in TYPE_LIST:
+			i = 1
+			while True:
+				source = download_source(driver, crawl_url % (city, _type, i))
+				isContinue = get_content(source)
+				if isContinue:
+					i += 1
+					continue
+				else:
+					break
